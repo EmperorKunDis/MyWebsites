5380ee465125208a64cb25c78b92425e21f3ce1d
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-04-01 15:26:34 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Author:    Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/index.html
    	modified:   src/index.js
    	modified:   src/sass/components/_dropdown.scss
    	modified:   src/sass/sections/_projects.scss
    	modified:   src/scripts/scripts.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,27 @@ import initScripts from "./scripts/scripts.js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+var myIndex = 0;
+    carousel();
+  
+      function carousel() {
+          var i;
+          var x = document.getElementsByClassName("mySlides");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "none";  
+          setTimeout(carousel, 4500);    
+        }
+    
+function myFunction() {
+        var x = document.getElementById("myLinks");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+      } 
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,86 +1,60 @@
-        //--------------------------- Dropdown menu -----------------------
+        //--------------------------- Dropdown menu ----------------------------------------
+
+        const buttonLanguageMenu = document.querySelector("#buttonLanguageMenu");
+        const buttonEnglish = document.querySelector("#buttonEnglish");
+        const buttonGerman = document.querySelector("#buttonGerman");
+        const buttonCzech = document.querySelector("#buttonCzech");
+        const languageMenuText = document.querySelector("#languageMenuText");
+        const languageGermanText = document.querySelector("#languageGermanText");
+        const languageCzechText = document.querySelector("#languagCzechText");
+        const home = document.querySelector("#home");
+        const aboutme = document.querySelector("#aboutme");
+        const contacts = document.querySelector("#contacts");
+        const projects = document.querySelector("#projects");
+        buttonEnglish.onclick = languageUpdateEN;
+        buttonGerman.onclick = languageUpdateDE;
+        buttonCzech.onclick = languageUpdateCZ;
 
 
-    
-        let home  = "Home";
-        let about  = "About Me";
-        let contact  = "Contacts";
-        let project  = "Projects";
 
-        let languageMenu = document.querySelector("#languageMenu");
-        let languageEnglish = document.querySelector("#languageEnglish");
-        let languageGerman = document.querySelector("#languageGerman");
-        let languageCzech = documen.querySelector("#languageCzech");
-
-        function updateWeb() {
-
-            if (language == "EN") {
-                languageMenu.innerText = "Language";
-                languageEnglish.innerText = "English";
-                languageCzech.innerText  = "Czech";
-                languageGerman.innerText  = "German";
-                home.innerText  = "Home";
-                about.innerText  = "About Me";
-                contact.innerText  = "Contacts";
-                project.innerText  = "Projects";
-            }
-            else if (language == "DE") {  
-                languageMenu.innerText = "Sprache";
-                languageEnglish.innerText = "Englisch";
-                languageCzech.innerText  = "Tschechisch";
-                languageGerman.innerText  = "Deutsch";
-                home  = "Startseite";
-                about  = "Über mich";
-                contact  = "Kontakte";
-                project  = "Projekte";
-            }
-            else if (language == "CZ") {
-                languageMenu.innerText = "Jazyk";
-                languageEnglish.innerText = "Anglický";
-                languageCzech.innerText  = "Český";
-                languageGerman.innerText  = "Nemecký";
-                home  = "Domaci obrazovka";
-                about  = "O mne";
-                contact  = "Kontakty";
-                project  = "Projekty";
-            } 
-        }
-
-        function translate() {
-            document.getElementById("languageMenu").innerText = languageMenu;
-            document.getElementById("languageEnglish").innerText = languageEnglish;
-            document.getElementById("languageCzech").innerText = languageCzech;
-            document.getElementById("languageGerman").innerText = languageGerman;
-            document.getElementById("home").innerText = home;
-            document.getElementById("about").innerText = about;
-            document.getElementById("contact").innerText = contact;
-            document.getElementById("project").innerText = project;
-        }
-
-        
-   
         function languageUpdateEN() {
-            var language = "EN"
-            updateWeb();
-
+          buttonLanguageMenu.innerText = "Language";
+          buttonEnglish.innerText = "English";
+          buttonGerman.innerText = "German";
+          buttonCzech.innerText = "Czech";
+          home.innerText = "Home";
+          aboutme.innerText = "About Me";
+          contacts.innerText = "Contacts";
+          projects.innerText = "Projects";    
         }
-        function languageUpdateDE() {
-            var language = "DE"
-            updateWeb();
-        }
+      function languageUpdateDE() {
+        buttonLanguageMenu.innerText = "Sprache";
+        buttonEnglish.innerText = "Englisch";
+        buttonCzech.innerText = "Tschechisch";
+        buttonGerman.innerText = "Deutsch";
+        home.innerText = "Startseite";
+        aboutme.innerText = "Über mich";
+        contacts.innerText = "Kontakte";
+        projects.innerText = "Projekte";
+      }
 
-        function languageUpdateCZ() {
-            var language = "CZ"
-            updateWeb();
-        }
-
+      function languageUpdateCZ() {
+        buttonLanguageMenu.innerText = "Jazyk";
+        buttonEnglish.innerText = "Anglický";
+        buttonGerman.innerText = "Nemecký";
+        buttonCzech.innerText = "Český";
+        home.innerText = "Domaci obrazovka";
+        aboutme.innerText = "O mne";
+        contacts.innerText = "Kontakty";
+        projects.innerText = "Projekty";
+      }
         function toggleDropdown() {
             var dropdown = document.getElementById("myDropdown");
             dropdown.classList.toggle("show");
           }
-          
+
           // Zavřít dropdown menu po kliknutí mimo něj
-      window.onclick = function(event) {
+          window.onclick = function(event) {
             if (!event.target.matches('.dropbtn')) {
               var dropdowns = document.getElementsByClassName("dropdown-content");
               var i;
@@ -94,26 +68,4 @@
           }
 
             
-    var myIndex = 0;
-    carousel();
-  
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "none";  
-    setTimeout(carousel, 4500);    
-    }
     
-function myFunction() {
-        var x = document.getElementById("myLinks");
-          if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-      } 
