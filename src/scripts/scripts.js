7142a57a3deb49ee92565522f5319c2cdd0a6b69
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-04-05 21:20:37 +0000

    rtry

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,7 +5,6 @@ const buttonEnglish = document.querySelector("#buttonEnglish");
 const buttonGerman = document.querySelector("#buttonGerman");
 const buttonCzech = document.querySelector("#buttonCzech");
 const languageMenuText = document.querySelector("#languageMenuText");
-const languageEnglishText = document.querySelector("#languageEnglishText");
 const languageGermanText = document.querySelector("#languageGermanText");
 const languageCzechText = document.querySelector("#languagCzechText");
 const home = document.querySelector("#home");
@@ -13,34 +12,52 @@ const aboutme = document.querySelector("#aboutme");
 const contacts = document.querySelector("#contacts");
 const project = document.querySelector("#project");
 
-function toggleLanguage(language) {
-  if (language === "EN") {
-    buttonLanguageMenu.innerText = "Language";
-    buttonEnglish.innerText = "English";
-    buttonGerman.innerText = "German";
-    buttonCzech.innerText = "Czech";
-    home.innerText = "Home";
-    aboutme.innerText = "About Me";
-    contacts.innerText = "Contacts";
-    project.innerText = "Projects";
-  }
-  if (language === "DE") {
-    buttonLanguageMenu.innerText = "Sprache";
-    buttonEnglish.innerText = "Englisch";
-    buttonCzech.innerText = "Tschechisch";
-    buttonGerman.innerText = "Deutsch";
-    home.innerText = "Startseite";
-    aboutme.innerText = "Über mich";
-    contacts.innerText = "Kontakte";
-    project.innerText = "Projekte";
-  } else if (language === "CS") {
-    buttonLanguageMenu.innerText = "Jazyk";
-    buttonEnglish.innerText = "Anglický";
-    buttonGerman.innerText = "Nemecký";
-    buttonCzech.innerText = "Český";
-    home.innerText = "Domaci obrazovka";
-    aboutme.innerText = "O mne";
-    contacts.innerText = "Kontakty";
-    project.innerText = "Projekty";
-  }
+var language = "EN";
+
+buttonEnglish.onclick = language = "EN";
+buttonGerman.onclick = language = "DE";
+buttonCzech.onclick = language = "CZ";
+
+switch (language) {
+  case "EN":
+    languageUpdateEN();
+    break;
+  case "DE":
+    languageUpdateDE();
+    break;
+  case "CZ":
+    languageUpdateCZ();
+    break;
+}
+
+function languageUpdateEN() {
+  buttonLanguageMenu.innerText = "Language";
+  buttonEnglish.innerText = "English";
+  buttonGerman.innerText = "German";
+  buttonCzech.innerText = "Czech";
+  home.innerText = "Home";
+  aboutme.innerText = "About Me";
+  contacts.innerText = "Contacts";
+  project.innerText = "Projects";
+}
+function languageUpdateDE() {
+  buttonLanguageMenu.innerText = "Sprache";
+  buttonEnglish.innerText = "Englisch";
+  buttonCzech.innerText = "Tschechisch";
+  buttonGerman.innerText = "Deutsch";
+  home.innerText = "Startseite";
+  aboutme.innerText = "Über mich";
+  contacts.innerText = "Kontakte";
+  project.innerText = "Projekte";
+}
+
+function languageUpdateCZ() {
+  buttonLanguageMenu.innerText = "Jazyk";
+  buttonEnglish.innerText = "Anglický";
+  buttonGerman.innerText = "Nemecký";
+  buttonCzech.innerText = "Český";
+  home.innerText = "Domaci obrazovka";
+  aboutme.innerText = "O mne";
+  contacts.innerText = "Kontakty";
+  project.innerText = "Projekty";
 }
