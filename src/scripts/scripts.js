b64d0a4c2aa782ff11660c69456aeee854bffe85
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-04-05 20:57:35 +0000

    d

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,53 +1,63 @@
-        //--------------------------- Dropdown menu ----------------------------------------
+//--------------------------- Dropdown menu ----------------------------------------
 
-        const buttonLanguageMenu = document.querySelector("#buttonLanguageMenu");
-        const buttonEnglish = document.querySelector("#buttonEnglish");
-        const buttonGerman = document.querySelector("#buttonGerman");
-        const buttonCzech = document.querySelector("#buttonCzech");
-        const languageMenuText = document.querySelector("#languageMenuText");
-        const languageGermanText = document.querySelector("#languageGermanText");
-        const languageCzechText = document.querySelector("#languagCzechText");
-        const home = document.querySelector("#home");
-        const aboutme = document.querySelector("#aboutme");
-        const contacts = document.querySelector("#contacts");
-        const project = document.querySelector("#project");
-        buttonEnglish.onclick = languageUpdateEN;
-        buttonGerman.onclick = languageUpdateDE;
-        buttonCzech.onclick = languageUpdateCZ;
+const buttonLanguageMenu = document.querySelector("#buttonLanguageMenu");
+const buttonEnglish = document.querySelector("#buttonEnglish");
+const buttonGerman = document.querySelector("#buttonGerman");
+const buttonCzech = document.querySelector("#buttonCzech");
+const languageMenuText = document.querySelector("#languageMenuText");
+const languageGermanText = document.querySelector("#languageGermanText");
+const languageCzechText = document.querySelector("#languagCzechText");
+const home = document.querySelector("#home");
+const aboutme = document.querySelector("#aboutme");
+const contacts = document.querySelector("#contacts");
+const project = document.querySelector("#project");
 
+var language = "EN";
 
+buttonEnglish.onclick = language = "EN";
+buttonGerman.onclick = language = "DE";
+buttonCzech.onclick = language = "CZ";
 
-        function languageUpdateEN() {
-          buttonLanguageMenu.innerText = "Language";
-          buttonEnglish.innerText = "English";
-          buttonGerman.innerText = "German";
-          buttonCzech.innerText = "Czech";
-          home.innerText = "Home";
-          aboutme.innerText = "About Me";
-          contacts.innerText = "Contacts";
-          project.innerText = "Projects";    
-        }
-      function languageUpdateDE() {
-        buttonLanguageMenu.innerText = "Sprache";
-        buttonEnglish.innerText = "Englisch";
-        buttonCzech.innerText = "Tschechisch";
-        buttonGerman.innerText = "Deutsch";
-        home.innerText = "Startseite";
-        aboutme.innerText = "Über mich";
-        contacts.innerText = "Kontakte";
-        project.innerText = "Projekte";
-      }
+switch (language) {
+  case "EN":
+    languageUpdateEN();
+    break;
+  case "DE":
+    languageUpdateDE();
+    break;
+  case "CZ":
+    languageUpdateCZ();
+    break;
+}
 
-      function languageUpdateCZ() {
-        buttonLanguageMenu.innerText = "Jazyk";
-        buttonEnglish.innerText = "Anglický";
-        buttonGerman.innerText = "Nemecký";
-        buttonCzech.innerText = "Český";
-        home.innerText = "Domaci obrazovka";
-        aboutme.innerText = "O mne";
-        contacts.innerText = "Kontakty";
-        project.innerText = "Projekty";
-      }
+function languageUpdateEN() {
+  buttonLanguageMenu.innerText = "Language";
+  buttonEnglish.innerText = "English";
+  buttonGerman.innerText = "German";
+  buttonCzech.innerText = "Czech";
+  home.innerText = "Home";
+  aboutme.innerText = "About Me";
+  contacts.innerText = "Contacts";
+  project.innerText = "Projects";
+}
+function languageUpdateDE() {
+  buttonLanguageMenu.innerText = "Sprache";
+  buttonEnglish.innerText = "Englisch";
+  buttonCzech.innerText = "Tschechisch";
+  buttonGerman.innerText = "Deutsch";
+  home.innerText = "Startseite";
+  aboutme.innerText = "Über mich";
+  contacts.innerText = "Kontakte";
+  project.innerText = "Projekte";
+}
 
-            
-    
+function languageUpdateCZ() {
+  buttonLanguageMenu.innerText = "Jazyk";
+  buttonEnglish.innerText = "Anglický";
+  buttonGerman.innerText = "Nemecký";
+  buttonCzech.innerText = "Český";
+  home.innerText = "Domaci obrazovka";
+  aboutme.innerText = "O mne";
+  contacts.innerText = "Kontakty";
+  project.innerText = "Projekty";
+}
