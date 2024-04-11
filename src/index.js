c1c8ac92401461ce3f71a4ab50db2f4d66161a81
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-04-11 07:10:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import initScrollReveal from "./src/scripts/scrollReveal";
-import initTiltEffect from "./src/scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./src/data/scrollRevealConfig";
-import initScripts from "./src/scripts/scripts.js"; 
-import {carousel} from "./src/scripts/projectsImg.js";
-import {myFunction} from "./src/scripts/myFunction.js";
 
-
-
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
-initScripts(carousel, myFunction,);
